8abc6b32ccf698323c31b6df6094aaa4d20f5256
Danish <danishsidd524@gmail.com>
2025-06-20 23:04:38 +0500

    add: testing terraform code

---
+++ testing/example/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+module "web_app" {
+  source = "../module"
+}
+
+output "instance_ip_addr" {
+  value = module.web_app.instance_ip_addr
+}
+
+output "url" {
+  value = "http://${module.web_app.instance_ip_addr}:8080"
+}
